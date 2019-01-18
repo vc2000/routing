ab221b0bb6b5bc7b5800149d4dabc8b0d1bbacb8
01653187 <01653187@corp.root>
2019-01-18 14:54:43 +0800

    added alert function

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import {Subject} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
+
 @Component({
   selector: 'app-page1',
   templateUrl: './page1.component.html',
   styleUrls: ['./page1.component.scss']
 })
 export class Page1Component implements OnInit {
+  private _success = new Subject<string>();
+
+  staticAlertClosed = false;
+  successMessage: string;
 
   constructor(
     private location: Location
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    setTimeout(() => this.staticAlertClosed = true, 20000);
+
+    this._success.subscribe((message) => this.successMessage = message);
+    this._success.pipe(
+      debounceTime(5000)
+    ).subscribe(() => this.successMessage = null);
+  }
+  
+  public changeSuccessMessage() {
+    this._success.next(`Message successfully changed.`);
   }
   goBack(): void {
     this.location.back();
